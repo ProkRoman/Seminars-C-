01c0f4e0405f60300589cb3496109ca6501aca4f
«prokoptsev_rn» <roma_prok@list.ru>
2022-10-19 19:34:37 +0500

    Add Task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,9 +1,2 @@
-﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
-// 456 -> 6 
-// 782 -> 2 
-// 918 -> 8
- 
-Console.Write("Введите трехзначное число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine(a % 10);
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
