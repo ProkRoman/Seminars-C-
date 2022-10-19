6cacd6059b0f7229d1b3d2abf379d487c1f95375
«prokoptsev_rn» <roma_prok@list.ru>
2022-10-19 23:03:42 +0500

    Add Hometask06

--- Hometask06/Program.cs
+++ Hometask06/Program.cs
@@ -2,3 +2,10 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+if (a % 2 == 0) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
