11bf586b882c5c847bcd5d008b88ed99f20e0341
«prokoptsev_rn» <roma_prok@list.ru>
2022-10-21 19:54:51 +0500

    Add Example011_ArrayLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int indexof(int[] CollectionExtensions, int find)
+{
+    int count = CollectionExtensions.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (CollectionExtensions[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = indexof(array, 4);
+Console.WriteLine(pos);
