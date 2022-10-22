c8d4b291f01f764027a5f4e24c87414ffb6b933a
«prokoptsev_rn» <roma_prok@list.ru>
2022-10-22 22:29:47 +0500

    Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// вывод случайного трехзначного числа и удаление второй цифры этого числа
+// 456 - 46
+// 782 - 72
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из диапазона 1 - 1000 -> {num}");
+
+int FirstLastNum(int number)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = number % 10;
+    int result = firstDigit * 10 + thirdDigit;
+    return result;
+}
+
+Console.WriteLine(FirstLastNum(num));
