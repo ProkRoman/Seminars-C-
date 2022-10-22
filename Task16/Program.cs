d3b2c4d076ba225b27a8648dd6e93c7fdc486bc3
«prokoptsev_rn» <roma_prok@list.ru>
2022-10-23 00:12:55 +0500

    Task16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -4,7 +4,16 @@
 // -4, 16 - да
 // 8, 9 - нет
 
-Сonsole.WriteLine("Введите первое число");
+bool Square(int number1, int number2)
+{
+    return number1 * number1 == number2 || number2 * number2 == number1;
+}
+
+Console.Write("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
+
+Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool square = Square(num1, num2);
+Console.Write(square ? "да": "нет");
