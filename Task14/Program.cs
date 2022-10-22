cada8f616e65ac3e0424ef74154a175955422ce4
«prokoptsev_rn» <roma_prok@list.ru>
2022-10-22 23:49:36 +0500

    Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,18 @@
+﻿// принимает на вход число и проверяет кратно
+//ли оно одновременно 7 и 23
+
+// 14 - нет
+// 46 - нет
+// 161 - да
+
+bool Multiple(int number)
+{
+    return number % 7 == 0 && number % 23 ==0;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool multiple = Multiple(num);
+
+Console.WriteLine(multiple ? "Кратно": "не кратно");
---
+++ Task16/Program.cs
@@ -0,0 +1,10 @@
+﻿// принимает на вход два числа и проверяет
+// является ли ли одно число квадратом другого
+// 5, 25 - да
+// -4, 16 - да
+// 8, 9 - нет
+
+Сonsole.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
