b59e9498b674a22c0487c88817070869270e16d7
«prokoptsev_rn» <roma_prok@list.ru>
2022-10-22 23:13:46 +0500

    Task12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -5,4 +5,22 @@
 // 34, 5 - не кратно, остаток 4
 // 16, 4 - кратно
 
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
+// bool Multiple(int number1, int number2)
+// {
+//     return number1 % number2 == 0;
+// }
+
+// bool multiple = Multiple(num1, num2);
+
+int Multiple(int number1, int number2)
+{
+    return number1 % number2;
+}
+
+int rest = Multiple(num1,num2);
+Console.WriteLine(rest ==0 ? "Кратно" : $"не кратно, остаток {rest}");
