d14052e88b638459561c26c855f1f5cd3ba68670
«prokoptsev_rn» <roma_prok@list.ru>
2022-10-23 17:25:11 +0500

    Hometask10

---
+++ Hometask10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа. Выполнить с помощью числовых операций 
+// (целочисленное деление, остаток от деления).
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SecondNum(int number)
+{
+    int secondDigit =(number / 10) % 10;
+    return secondDigit;
+}
+
+Console.WriteLine(SecondNum(num));
