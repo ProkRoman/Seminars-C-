6b1b6fd0f4da4ae3e8032469164b64fad2030936
«prokoptsev_rn» <roma_prok@list.ru>
2022-10-23 18:58:37 +0500

    Hometask15

--- Hometask15/Program.cs
+++ Hometask15/Program.cs
@@ -3,3 +3,20 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Write("Введите цифру обозначающую день недели: ");
+int daynumber = Convert.ToInt32(Console.ReadLine());
+
+
+void CheckingTheDayofTheWeek (int daynumber) {
+    if (daynumber == 6 || daynumber == 7)
+    {
+        Console.WriteLine("(этот день выходной) -> да");
+    }
+    else if (daynumber < 1 || daynumber > 7) {
+        Console.WriteLine("(такого дня недели нет");
+    }
+    else Console.WriteLine("(этот день не выходной) -> нет");
+}    
+
+CheckingTheDayofTheWeek(daynumber);
