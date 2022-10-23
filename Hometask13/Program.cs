cbca448f05e4535baa9a591e2a1cc48f858b4a31
«prokoptsev_rn» <roma_prok@list.ru>
2022-10-23 18:05:39 +0500

    Hometask13

--- Hometask13/Program.cs
+++ Hometask13/Program.cs
@@ -3,3 +3,18 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+int ThirdNum(int number)
+{
+    int thirdDigit = number % 10;
+    return thirdDigit;
+}
+int rest = ThirdNum(num);
+
+if (num > 100) Console.WriteLine(rest);
+else Console.WriteLine("третьей цифры нет");
