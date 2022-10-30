0dbd9f5e7bfc6d699c285db03457f6ce5ea6e648
«prokoptsev_rn» <roma_prok@list.ru>
2022-10-30 21:14:54 +0500

    Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу которая принимает на вход число 
+// (N) и выдает таблцу квадратов чисел от 1 до N
+
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+
+void Table (int n)
+{
+    if (n > 0)
+    {
+        int index = 1;
+        while (index <= n)
+        {
+            Console.WriteLine($"{index} {index*index}" );
+            index++;
+        }
+    }
+    else Console.WriteLine("Некорректное число");
+}
+
+Console.WriteLine("Введите число N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Table(n);
+
