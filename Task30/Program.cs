eac923a815bef2a7d64e8a573c946526663c2e2b
«prokoptsev_rn» <roma_prok@list.ru>
2022-11-09 20:32:30 +0500

    Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -4,3 +4,19 @@
 
 //  [1,0,1,1,0,1,0,0]
 
+
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i]= new Random().Next(0,2);
+        if (i!=arr.Length -1)Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]},");
+    }
+    Console.Write("]");
+}
+
+int [] array = new int[8];
+PrintArray(array);
