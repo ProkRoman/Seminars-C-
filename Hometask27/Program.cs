1efafb0f26a6063d48f9445d2be7d8953b664eb1
«prokoptsev_rn» <roma_prok@list.ru>
2022-11-10 21:13:44 +0500

    Hometask27

---
+++ Hometask27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: 
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumDigits(int number)
+{
+    int count = 0;
+
+    while (number>0)
+    {
+        
+        int num = number % 10;
+        number = number / 10;
+        count = count + num;
+
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumDigits = SumDigits(number);
+Console.WriteLine($"сумма чисел в числе: {sumDigits}");
