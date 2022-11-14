6803942cd76636b670460b973f2fedc490cc9098
«prokoptsev_rn» <roma_prok@list.ru>
2022-11-14 20:36:01 +0500

    Task33

--- Task33/Program.cs
+++ Task33/Program.cs
@@ -4,3 +4,39 @@
 //  4; массив [6, 7, 19, 345, 3] -> нет 
 //  3; массив [6, 7, 19, 345, 3] -> да
  
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < Array.MaxLength - 1) Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+
+bool FindNumberArray(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number) return true; 
+    }
+    return false;
+}
+
+int[] arr = CreateArrayRndInt(12, -99, 100);
+PrintArray(arr);
+bool result = FindNumberArray(arr, 5);
+
+
+Console.WriteLine(result ? "Да" : "Нет");
