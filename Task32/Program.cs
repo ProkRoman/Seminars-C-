8fe2ca1a3d45251cadbf0957bb8da9b4c4feb885
«prokoptsev_rn» <roma_prok@list.ru>
2022-11-14 20:23:48 +0500

    Task32

--- Task32/Program.cs
+++ Task32/Program.cs
@@ -3,3 +3,40 @@
 // положительные элементы замените на соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < Array.MaxLength - 1) Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+
+void MirrorArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
+
+int[] arr = CreateArrayRndInt(12, -9, 9);
+PrintArray(arr);
+
+
+MirrorArray(arr);
+
+Console.WriteLine($"Замена элементов массива с противоположным знаком.");
+PrintArray(arr);
