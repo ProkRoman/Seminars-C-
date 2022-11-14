b5ab7d579976d74298c8dcbd52caf3ff6a4eea87
«prokoptsev_rn» <roma_prok@list.ru>
2022-11-14 22:51:17 +0500

    Task35

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -7,3 +7,41 @@
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < Array.MaxLength - 1) Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+
+int GetCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array [i]<=99) 
+        count++;
+    }
+    return count;
+}
+
+
+int[] arr = CreateArrayRndInt(123, 0, 200);
+// PrintArray(arr);
+
+int getCount= GetCount(arr);
+Console.WriteLine($"количество элементов массива, значения которых лежат в отрезке [10,99] = {getCount}");
