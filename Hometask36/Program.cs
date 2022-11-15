9cbf8bf32d581aec9992e0a4976f066ccdc7d955
«prokoptsev_rn» <roma_prok@list.ru>
2022-11-15 22:12:03 +0500

    Hometask36

--- Hometask36/Program.cs
+++ Hometask36/Program.cs
@@ -3,3 +3,44 @@
 // Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19 
 // [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int AdditionElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i=i+2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+int[] arr = CreateArrayRndInt(4, -10, 10);
+PrintArray(arr);
+
+int additionElements = AdditionElements(arr);
+Console.WriteLine($"сумма элементов, стоящих на нечётных позициях = {additionElements}");
+
+
+
