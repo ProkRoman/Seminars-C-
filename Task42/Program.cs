ce222c37029e5efd438dac691f5dd111d6b6e03a
«prokoptsev_rn» <roma_prok@list.ru>
2022-11-20 17:25:04 +0500

    Task42

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -4,3 +4,19 @@
 // 45 -> 101101 
 // 3 -> 11
 // 2 -> 10
+
+Console.WriteLine("введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string DecToBin(int num)
+{
+    string result = "";
+    while (num > 0)
+    {
+        result = $"{num % 2}{result}";
+        num = num/2;
+    }
+    return result;
+}
+
+Console.WriteLine(DecToBin(number));
